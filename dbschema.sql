39430d3134fdaa30eed7b7ebb1b571646de06976
Amber Sharma <shrama.amber.p@gmail.com>
2019-02-07 12:28:28 -0800

    Sets up schema and reltracks

---
+++ dbschema.sql
@@ -0,0 +1,51 @@
+CREATE DATABASE IF NOT EXISTS zoundloud;
+
+CREATE TABLE Artists if not exists {
+  id int not null auto_increment,
+  name varchar(35) not null, 
+  avatar_pic_url varchar(110) not null,
+  pro_user boolean not null, 
+  location varchar(100)
+}
+
+CREATE TABLE Playlists if not exists {
+  playid int not null auto_increment,
+  name varchar(60) not null,
+  play_pic_url varchar(110),
+  author_id int not null
+}
+
+CREATE TABLE Songs if not exists {
+  song_id int not null auto_increment, 
+  artistId int not null,
+  album_pic varchar(110) not null, 
+  artist_pic varchar(110) not null,
+  numComments int,
+  numPlays int, 
+  numLikes int,
+  numReposts int
+}
+
+CREATE TABLE Likes if not exists {
+  id int not null auto_increment,
+  userid int not null,
+  songid || playlistid int not null
+}
+
+CREATE TABLE Followers if not exists {
+  id int not null auto_increment, 
+
+}
+
+CREATE TABLE Comments if not exists {
+  id int not null auto_increment,
+  userid int not null,
+  songid int not null
+}
+
+CREATE TABLE Users if not exists {
+  id int not null auto_increment,
+  username varchar(40) not null,
+  numfollowers int,
+  location varchar(40)
+}
