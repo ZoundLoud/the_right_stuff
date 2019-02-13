a17235b2ea885984f6388ec53e0edda4aa94c203
Amber Sharma <shrama.amber.p@gmail.com>
2019-02-12 17:15:36 -0800

    Removes prop types import

--- client/components/RelSong.jsx
+++ client/components/RelSong.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types'; // ES6
-import RelatedSongsList from './RelSongBox.jsx/index.js';
-import Epony from './Eponymous.jsx';
 
 
 function RelSong({ eachSong, key }) {
   return (
-    <li>{eachSong.songName}</li>
+    <li key={key}>{eachSong.songName}</li>
   );
 }
 
